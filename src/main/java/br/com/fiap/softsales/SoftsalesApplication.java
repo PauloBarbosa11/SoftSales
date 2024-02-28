b7721f1a43a10631340cf85784bb527058579bc7
Cassiyu <sakaicassio@gmail.com>
2024-02-28 13:04:50 -0300

    Criação do Projeto Spring

---
+++ src/main/java/br/com/fiap/softsales/SoftsalesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.softsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftsalesApplication.class, args);
+	}
+
+}
