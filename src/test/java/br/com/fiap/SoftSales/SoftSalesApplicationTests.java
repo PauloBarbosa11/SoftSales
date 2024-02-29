5abb9e15e15dd42a138ce48fa90146b169f8f6f0
PauloBarbosa11 <paulobarbosan2005@gmail.com1>
2024-02-29 10:05:52 -0300

    Criação do Projeto Spring

---
+++ src/test/java/br/com/fiap/SoftSales/SoftSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.SoftSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
