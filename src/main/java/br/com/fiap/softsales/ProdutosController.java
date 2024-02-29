a2f8fb363102eacb0dcc11ba1879ba2a65075988
Cassiyu <sakaicassio@gmail.com>
2024-02-29 10:09:26 -0300

    Produtos Controller

---
+++ src/main/java/br/com/fiap/softsales/ProdutosController.java
@@ -0,0 +1,31 @@
+package br.com.fiap.softsales;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ProdutosController {
+
+    @RequestMapping(
+        method= RequestMethod.GET,
+        path = "/produtos",
+        produces = "application/json"
+    )
+
+    @ResponseBody
+    public String index(){
+        return"""
+                [
+                    {
+                        "id": 1,
+                        "nome": "Mouse Logitech G Pro X",
+                        "valor": 700.00
+                    }
+                ]
+                """;
+    }
+
+
+}
